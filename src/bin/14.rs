059c569cc3ae4aef5c2658a40be7529cd4c6a757
jpbougie <jean-philippe@demarque.com>
2021-12-14 11:22:34 -0500

    Day 14 part 2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -5,10 +5,11 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let input = include_str!("../../inputs/14.txt");
     let mut parts = input.split("\n\n");
-    let mut template = String::from(parts.next().unwrap());
-    let rules: HashMap<&str, String> = parts
-        .next()
-        .unwrap()
+    let original_template = String::from(parts.next().unwrap());
+    let mut template = original_template.clone();
+
+    let rules_input = parts.next().unwrap();
+    let rules: HashMap<&str, String> = rules_input
         .lines()
         .map(|line| {
             let mut parts = line.split(" -> ");
@@ -40,21 +41,50 @@ fn main() -> Result<()> {
             min = freq;
         }
     }
-    println!("Part 1: {}", max - min);
+    println!("Part 1: {}", max + 1 - min);
 
-    for i in 0..30 {
-        println!("{}", i);
-        template = apply(&template, &rules);
-    }
+    let rules: Rules = rules_input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(" -> ");
+            let from = parts.next().unwrap();
+            let mut from_chars = from.chars();
+            let from_first = from_chars.next().unwrap();
+            let from_second = from_chars.next().unwrap();
+            let to_char = parts.next().unwrap().chars().next().unwrap();
+            (
+                (from_first, from_second),
+                ((from_first, to_char), (to_char, from_second)),
+            )
+        })
+        .collect();
 
-    let mut char_freqs: HashMap<char, u64> = HashMap::with_capacity(26);
-    for ch in template.chars() {
-        let entry = char_freqs.entry(ch).or_default();
+    let mut freqs: HashMap<(char, char), u128> = HashMap::new();
+    for wnd in original_template.chars().collect::<Vec<_>>().windows(2) {
+        let entry = freqs.entry((wnd[0], wnd[1])).or_default();
         *entry += 1;
     }
 
-    let mut max = 0u64;
-    let mut min = u64::MAX;
+    for _i in 0..40 {
+        freqs = apply_optimized(&freqs, &rules);
+    }
+
+    let mut char_freqs: HashMap<char, u128> = HashMap::with_capacity(26);
+    for ((cha, _chb), count) in freqs.into_iter() {
+        let entry = char_freqs.entry(cha).or_default();
+        *entry += count;
+        // let entry = char_freqs.entry(chb).or_default();
+        // *entry += count;
+    }
+
+    // Add 1 to the last char
+    let entry = char_freqs
+        .entry(original_template.chars().last().unwrap())
+        .or_default();
+    *entry += 1;
+
+    let mut max = 0u128;
+    let mut min = u128::MAX;
     for (_ch, freq) in char_freqs.into_iter() {
         if freq > max {
             max = freq;
@@ -65,6 +95,23 @@ fn main() -> Result<()> {
     }
     println!("Part 2: {}", max - min);
     Ok(())
+}
+
+type Rules = HashMap<(char, char), ((char, char), (char, char))>;
+fn apply_optimized(
+    freqs: &HashMap<(char, char), u128>,
+    rules: &Rules,
+) -> HashMap<(char, char), u128> {
+    let mut new_freqs = HashMap::with_capacity(freqs.len());
+    for (pair, v) in freqs {
+        let (a, b) = rules.get(pair).unwrap();
+        let entry = new_freqs.entry(*a).or_default();
+        *entry += v;
+        let entry = new_freqs.entry(*b).or_default();
+        *entry += v;
+    }
+
+    new_freqs
 }
 
 fn apply(template: &str, rules: &HashMap<&str, String>) -> String {
